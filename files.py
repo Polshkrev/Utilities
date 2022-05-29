f12113729d6c1cabf7660d1939936d9b2cc3900b
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2022-05-28 23:48:37 -0400

    Update files.py

---
+++ files.py
@@ -0,0 +1,16 @@
+import zipfile
+
+def zip_files(name: str, files: list[str]):
+
+    zip_name = f"{name}.zip"
+    
+    with zipfile.ZipFile(zip_name, "w") as f:
+        for file in files:
+            f.write(file)
+
+def extract_zip_file(zipped_file: str) -> None:
+
+    file = f"{zipped_file}.zip"
+
+    with zipfile.ZipFile(file, "r") as f:
+        f.extractall(zipped_file)
