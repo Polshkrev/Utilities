854051deff921e7a61f227bc5685532d80281291
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2022-05-29 00:11:09 -0400

    Update display.py

--- display.py
+++ display.py
@@ -1,5 +1,8 @@
+import time
+
+
 def progress_bar(initial_value: int, total: int) -> None:
-    """Decorator to display a progress bar when a function runs."""
+    """Display a progress bar when a function runs."""
     percent = int(100 * (initial_value / float(total)))
     bar = "█" * percent + "-" * (100 - percent)
     print(f"\r|{bar}| {percent:.2f}%", end="\r")
