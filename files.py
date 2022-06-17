8cf1ebc6c30973338bcbdd2bcba47ac3acfc164f
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2022-06-17 12:27:22 -0400

    Update files.py

--- files.py
+++ files.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import zipfile
 
 def zip_files(name: str, files: list[str]) -> None:
@@ -14,3 +15,13 @@ def extract_zip_file(zipped_file: str) -> None:
 
     with zipfile.ZipFile(file, "r") as f:
         f.extractall(zipped_file)
+
+def compare(file1: str, file2: str) -> Optional[bool]:
+
+    with open(file1, "r", encoding='utf-8') as f:
+        contents1 = f.readlines()
+    with open(file2, "r", encoding='utf-8') as f:
+        contents2 = f.readlines()
+    if contents1 == contents2:
+        return True
+    print("".join([f"Difference [{contents2.index(line) + 1}]: {line}" for line in contents2 if line not in contents1]))
