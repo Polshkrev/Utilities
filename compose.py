d594fab9f85d7528f849db831c48590b553b743d
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2022-06-17 12:06:38 -0400

    Update compose.py

--- compose.py
+++ compose.py
@@ -5,3 +5,6 @@ ComposableFunction = Callable[..., Any]
 
 def compose(*functions: ComposableFunction) -> ComposableFunction:
     return functools.reduce(lambda f, g: lambda x: g(f(x)), functions)
+
+def args(*arguments: str) -> list[str]:
+    return [arg for arg in arguments]
