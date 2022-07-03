8f54ae0f83762997278608af2f9a89e619617bdd
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2022-07-03 16:28:48 -0400

    Update types.py

--- types.py
+++ types.py
@@ -1,3 +1,8 @@
+from typing import Callable, Awaitable, Any
+
 JSON = int | str | float | bool | None | dict[str, "JSON"] | list["JSON"]
 JSONObject = dict[str, JSON]
 JSONList = list[JSON]
+
+ComposableFunction = Callable[..., Any]
+AwaitableFunction = Awaitable[Any]
