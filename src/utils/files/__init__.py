ff3772a217a5ad1f2cb1b46555f77cda47b59313
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2023-01-01 14:39:50 -0500

    Revert "added file utilities"

--- src/utils/files/__init__.py
+++
@@ -1,2 +0,0 @@
-from src.utils.files.file import zip, extract, compare
-from src.utils.files.preprocess import to_snake_case
--- src/utils/files/preprocess.py
+++
@@ -1,4 +0,0 @@
-import re
-
-def to_snake_case(text: list[str]) -> list[str]:
-    return [re.sub(r'(.)([A-Z][a-z]+)', r'\1_\2', char).lower() for char in text]
