711460f04c1d13a2320fbcbfe81a8a425f1c4084
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2023-01-27 09:28:59 -0500

    import model change

---
+++ pkutils/__init__.py
@@ -0,0 +1,2 @@
+from .aliases import JSON, JSONList, JSONObject, ComposableFunction, AwaitableFunction, Grid
+from .log import Logger, LoggingLevel
--- src/pkutils/globals/__init__.py
+++
@@ -1,2 +0,0 @@
-from ..globals.log import Logger, LoggingLevel
-from ..globals.aliases import JSON, JSONList, JSONObject, AwaitableFunction, ComposableFunction, Grid
