e83ffd4658813421ec5247f8763c64ca7a35835e
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2023-02-16 17:48:49 -0500

    dev naming update

--- src/pkutils/__init__.py
+++
@@ -1,2 +0,0 @@
-from pkutils.aliases import JSON, JSONList, JSONObject, ComposableFunction, AwaitableFunction, Grid
-from pkutils.log import Logger, LoggingLevel
---
+++ src/polutils/__init__.py
@@ -0,0 +1,2 @@
+from polutils.aliases import JSON, JSONList, JSONObject, ComposableFunction, AwaitableFunction, Grid
+from polutils.log import Logger, LoggingLevel
