9278e88cbf38f455f99fa84257535e5bcd68a915
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2023-02-16 17:58:22 -0500

    Update __init__.py

--- src/polutils/__init__.py
+++ src/polutils/__init__.py
@@ -1,2 +1,3 @@
 from polutils.aliases import JSON, JSONList, JSONObject, ComposableFunction, AwaitableFunction, Grid
 from polutils.log import Logger, LoggingLevel
+from polutils import files, funcs, patterns
